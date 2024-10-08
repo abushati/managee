f69986fe92c17a85946c1bde1ddbe1714e2e277b
abushati <arvid.b901@gmail.com>
2024-10-07 22:55:05 -0400

    Schedule clean up

--- routes/routes.go
+++ routes/routes.go
@@ -2,63 +2,64 @@ package routes
 
 import (
 	"fmt"
-    "net/http"
-    "strings"
-	"strconv"
-    "github.com/gin-gonic/gin"
 	"managee/structs"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func string_to_int(stringInt string) (int, error) {
 	i, err := strconv.Atoi(stringInt)
 	if err != nil {
-        fmt.Println("Error converting string to int:", err)
-        return 0, err
-    }
+		fmt.Println("Error converting string to int:", err)
+		return 0, err
+	}
 	return i, nil
 }
 
 // SetupRoutes sets up the routes for the application
 func SetupRoutes(r *gin.Engine) {
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello, world!",
-            "upper":   strings.ToUpper("hello"),
-        })
-    })
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, world!",
+			"upper":   strings.ToUpper("hello"),
+		})
+	})
 
-    r.GET("/greet", func(c *gin.Context) {
-        name := c.Query("name")
-        if name == "" {
-            name = "stranger"
-        }
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello, " + name + "!",
-        })
-    })
+	r.GET("/greet", func(c *gin.Context) {
+		name := c.Query("name")
+		if name == "" {
+			name = "stranger"
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, " + name + "!",
+		})
+	})
 
 	r.POST("/employee", func(c *gin.Context) {
-        var user structs.User // Use the User struct
-        if err := c.ShouldBindJSON(&user); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Data received for " + user.Name,
-            "email":   user.Email,
-            "age":     user.Age,
-        })
-    })
+		var user structs.Employee // Use the User struct
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Data received for " + user.Name,
+			"email":   user.Email,
+			"age":     user.Age,
+		})
+	})
 
 	r.GET("/employee/:e_id/schedule", func(c *gin.Context) {
 		idParam := c.Param("e_id")
-		dayQ := c.Query("day")
-		weekQ := c.Query("week")
-		yearQ := c.Query("year")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Error" + fmt.Sprintf("%+v", yearQ,)+ fmt.Sprintf("%+v", weekQ,)+ fmt.Sprintf("%+v", dayQ,),
-		})
-		return
+		// dayQ := c.Query("day")
+		// weekQ := c.Query("week")
+		// yearQ := c.Query("year")
+		// c.JSON(http.StatusOK, gin.H{
+		// 	"message": "Error" + fmt.Sprintf("%+v", yearQ) + fmt.Sprintf("%+v", weekQ) + fmt.Sprintf("%+v", dayQ),
+		// })
+		// return
 
 		idUint, err := string_to_int(idParam)
 		if err != nil {
@@ -68,18 +69,18 @@ func SetupRoutes(r *gin.Engine) {
 			return
 		}
 
-		user := structs.Get_user(idUint)
-		u_sch := structs.UserSch{UserID: user.ID, Day: structs.Saturday, Week: 4, Year: 2024, StartTime: 12123432, EndTime: 12123432}
+		user := structs.GetEmployee(idUint)
+		u_sch := user.GetSchedule()
 
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Data received for " + fmt.Sprintf("%+v", u_sch),
-        })
-    })
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Data received for " + fmt.Sprintf("%+v", u_sch),
+		})
+	})
 
 	r.GET("/employee/:e_id", func(c *gin.Context) {
 		idParam := c.Param("e_id")
 		c.JSON(http.StatusOK, gin.H{
-            "message": idParam,
-        })
+			"message": idParam,
+		})
 	})
 }
--- structs/structs.go
+++ structs/structs.go
@@ -2,21 +2,28 @@ package structs
 
 // User represents a user in the application
 
-type User struct {
-    ID    int   `json:"id" binding:"required"` // Unique identifier
-    Name  string `json:"name" binding:"required"` // User's name
-    Email string `json:"email" binding:"required,email"` // User's email
-    Age   int    `json:"age" binding:"required,min=0"` // User's age
+type Employee struct {
+	ID      int    `json:"id" binding:"required"`          // Unique identifier
+	Name    string `json:"name" binding:"required"`        // User's name
+	Email   string `json:"email" binding:"required,email"` // User's email
+	Age     int    `json:"age" binding:"required,min=0"`   // User's age
+	StoreID int
 }
 
-func Get_user(userId int) User{
-	user := User{
-		ID: userId,
-		Name: "test user",
+func GetEmployee(eId int) Employee {
+	user := Employee{
+		ID:    eId,
+		Name:  "test user",
 		Email: "test@user.com",
-		Age: 12,
+		Age:   12,
 	}
 	return user
+}
+
+func (employee Employee) GetSchedule() EmployeeSchedule {
+	storeId := employee.StoreID
+	eId := employee.ID
+	return EmployeeSchedule{StoreID: storeId, EmployeeID: eId, Day: Sunday, Week: 3, Year: 2024, StartTime: 1243, EndTime: 2345}
 }
 
 type DayOfWeek int
@@ -24,23 +31,21 @@ type WeekOfYear int
 type Year int
 
 const (
-    Saturday DayOfWeek = iota
-    Sunday
-    Monday
-    Tuesday
+	Saturday DayOfWeek = iota
+	Sunday
+	Monday
+	Tuesday
 	Wednesday
 	Thrusday
 	Friday
 )
 
-type UserSch struct {
-	UserID int
-	Day DayOfWeek
-	Week WeekOfYear
-	Year Year
-	StartTime int
-	EndTime int
+type EmployeeSchedule struct {
+	StoreID    int
+	EmployeeID int
+	Day        DayOfWeek
+	Week       WeekOfYear
+	Year       Year
+	StartTime  int
+	EndTime    int
 }
-
-
-
